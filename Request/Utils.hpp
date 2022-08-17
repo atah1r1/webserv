8414c0c486fdd41787d19131c621b99e19ad8bdc
abdlalisalmi <abdlalisalmi@gmail.com>
2022-08-17 23:59:19 +0100

    finish the parsing of the unchunked request body

--- Request/Utils.hpp
+++ Request/Utils.hpp
@@ -6,7 +6,7 @@
 /*   By: aes-salm <aes-salm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 12:22:25 by aes-salm          #+#    #+#             */
-/*   Updated: 2022/08/08 11:58:00 by aes-salm         ###   ########.fr       */
+/*   Updated: 2022/08/11 13:24:34 by aes-salm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 
 #include "Request.hpp"
 
+void parseRequest(Request &request, std::string buffer);
 void parseFirstLine(std::string line, Request *request);
 void parseHeaders(std::string line, Request *request);
 void parseBody(std::string line, Request *request);
-void parseRequest(Request &request, std::string buffer);
 
 #endif
