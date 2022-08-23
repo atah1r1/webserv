1a6d1ba2b2388203cdcb8c5d9a08313429aedf24
atah1r1 <atahiri@student.1337.ma>
2022-08-23 12:19:08 +0100

    add upload script

---
+++ www/upload/upload.php
@@ -0,0 +1,6 @@
+<?php
+$target_dir = "/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+?>
